bd88273b157fbef071ebf97ad6eb4a6cec00018a
minatwincle <hetaco3710@gmail.com>
2023-06-28 20:29:29 +0900

    headerのfixedとスムーススクロールを実装

--- demo-site01/js/main.js
+++ demo-site01/js/main.js
@@ -2,14 +2,34 @@
 const fixedTarget = document.getElementById('head');
 window.addEventListener('scroll', () => {
   const offset = window.scrollY;
-  if (offset > 600) {
+  if (offset > 960) {
+    fixedTarget.classList.add('fixed-on');
     fixedTarget.classList.add('slide-in');
-  } else if (offset <= 600) {
+  } else {
     fixedTarget.classList.remove('slide-in');
   }
   if (offset > 160) {
     fixedTarget.classList.add('slide-out');
   } else {
+    fixedTarget.classList.remove('fixed-on');
     fixedTarget.classList.remove('slide-out');
   }
+});
+
+// スムーススクロール
+const links = document.querySelectorAll('a[href^="#"');
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const rect = targetElement.getBoundingClientRect().top;
+    const offset = window.pageYOffset;
+    const gap = 160;
+    const target = rect + offset - gap;
+    window.scrollTo({
+      top: target,
+      behavior: 'smooth',
+    });
+  });
 });
