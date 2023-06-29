64bd6a19bd19891e9ff40afb1952218c32b30fc1
minatwincle <hetaco3710@gmail.com>
2023-06-30 02:30:19 +0900

    スライダーの実装

--- demo-site01/js/main.js
+++ demo-site01/js/main.js
@@ -1,3 +1,37 @@
+// ロード中画面
+function disableScroll() {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+  window.onscroll = function () {
+    window.scrollTo(0, 0);
+  };
+  setTimeout(function () {
+    window.onscroll = null;
+  }, 2800);
+}
+const loadPhaze = document.getElementById('load');
+const message = document.getElementsByClassName('fv__text')[0];
+const wrapper = document.getElementsByClassName('wrapper')[0];
+window.addEventListener('load', () => {
+  disableScroll();
+  loadPhaze.classList.add('on');
+  message.classList.add('out');
+  setTimeout(function () {
+    message.classList.add('fade-in');
+  }, 600);
+  setTimeout(function () {
+    loadPhaze.classList.add('off');
+  }, 2000);
+  setTimeout(function () {
+    loadPhaze.classList.remove('on');
+    loadPhaze.classList.remove('off');
+    loadPhaze.style.display = 'none';
+    message.classList.remove('out');
+    message.classList.remove('fade-in');
+  }, 2800);
+});
+
 // スクロールするとメニューをfixed表示にする
 const fixedTarget = document.getElementById('head');
 window.addEventListener('scroll', () => {
