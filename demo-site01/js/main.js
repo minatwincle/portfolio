9f0794445683399efbdf3711e7057014c961e5a6
minatwincle <hetaco3710@gmail.com>
2023-07-11 12:45:31 +0900

    jsにコメント挿入

--- demo-site01/js/main.js
+++ demo-site01/js/main.js
@@ -1,4 +1,7 @@
+//--------------------------------------------------------
 // ロード中画面
+//--------------------------------------------------------
+//ロード画面中はスクロールを固定する
 function disableScroll() {
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
@@ -31,7 +34,9 @@ window.addEventListener('load', () => {
   }, 2800);
 });
 
+//--------------------------------------------------------
 // スクロールするとメニューをfixed表示にする
+//--------------------------------------------------------
 const fixedTarget = document.getElementById('head');
 window.addEventListener('scroll', () => {
   const offset = window.scrollY;
@@ -49,7 +54,9 @@ window.addEventListener('scroll', () => {
   }
 });
 
+//--------------------------------------------------------
 // スムーススクロール
+//--------------------------------------------------------
 const links = document.querySelectorAll('a[href^="#"');
 links.forEach((link) => {
   link.addEventListener('click', (event) => {
@@ -67,18 +74,23 @@ links.forEach((link) => {
   });
 });
 
+//--------------------------------------------------------
 // バーガーメニュー
+//--------------------------------------------------------
 const burgerMenu = document.getElementsByClassName('burger')[0];
 const btns = document.getElementsByClassName('container--nav--btns')[0];
 const navList = document.getElementById('nav');
 const navContainer = [burgerMenu, btns, navList];
+//表示関数
 function appear() {
   btns.classList.add('on');
   navList.classList.add('on');
+  // アニメーションを発火させるために後回し
   setTimeout(function () {
     btns.classList.toggle('appear');
     navList.classList.toggle('appear');
-  }, 1);
+  }, 0);
+  //アニメーション中は入力を受け付けない
   navContainer.forEach((navItem) => {
     navItem.setAttribute('style', 'pointer-events:none');
     setTimeout(function () {
@@ -86,6 +98,7 @@ function appear() {
     }, 300);
   });
 }
+//非表示関数
 function disappear() {
   if (btns.classList.contains('on')) {
     setTimeout(function () {
@@ -94,12 +107,14 @@ function disappear() {
     }, 300);
   }
 }
+//クリックしたときに表示・非表示を切り替える
 navContainer.forEach((navItem) => {
   navItem.addEventListener('click', function () {
     disappear();
     appear();
   });
 });
+// スクロールした際、メニューを閉じる
 window.addEventListener('scroll', function () {
   navContainer.forEach((navItem) => {
     if (btns.classList.contains('on')) {
